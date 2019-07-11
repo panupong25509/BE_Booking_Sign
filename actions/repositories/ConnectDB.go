a9b5c1365fea31c50f0048b5e1653e0f290b121b
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-07-11 13:55:02 +0700

    [ADD] model error

--- actions/repositories/ConnectDB.go
+++ actions/repositories/ConnectDB.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"github.com/JewlyTwin/be_booking_sign/models"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
 )
@@ -8,7 +9,7 @@ import (
 func ConnectDB(c buffalo.Context) (*pop.Connection, interface{}) {
 	db, ok := c.Value("tx").(*pop.Connection)
 	if !ok {
-		return nil, 500
+		return nil, models.Error{500, "can't connect db"}
 	}
 	return db, nil
 }
