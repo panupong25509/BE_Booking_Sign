7e15e43f4ef9b74114da2e1eac4f550fc333a766
JewlyTwin <l2jew555@hotmail.com>
2019-07-25 18:14:53 +0700

    [UPDATE] func getuserbyusername

--- actions/handlers/User.go
+++ actions/handlers/User.go
@@ -22,3 +22,11 @@ func GetUserById(c buffalo.Context) error {
 	}
 	return c.Render(200, r.JSON(success))
 }
+func GetUser(c buffalo.Context) error {
+	success, err := repositories.GetUser(c)
+	if err != nil {
+		status := err.(models.Error)
+		return c.Render(status.Code, r.JSON(status))
+	}
+	return c.Render(200, r.JSON(success))
+}
--- actions/repositories/User.go
+++ actions/repositories/User.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"log"
+
 	"github.com/JewlyTwin/be_booking_sign/models"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gofrs/uuid"
@@ -39,4 +41,36 @@ func GetUserById(c buffalo.Context) (interface{}, interface{}) {
 		return nil, models.Error{400, "ไม่มีผู้ใช้นี้ใน database"}
 	}
 	return user, nil
+}
+
+func GetUserByIduuid(c buffalo.Context, id uuid.UUID) (interface{}, interface{}) {
+	db, err := ConnectDB(c)
+	if err != nil {
+		return nil, err
+	}
+	user := models.User{}
+	err = db.Find(&user, id)
+	log.Print(user)
+	if err != nil {
+		return nil, models.Error{400, "ไม่มีผู้ใช้นี้ใน"}
+	}
+	return user, nil
+}
+
+func GetUserByUsername(c buffalo.Context) (interface{}, interface{}) {
+	db, err := ConnectDB(c)
+	if err != nil {
+		return nil, err
+	}
+	data := DynamicPostForm(c)
+	username := data["username"]
+	user := models.Users{}
+	if username == nil {
+		return nil, models.Error{400, "ไม่มีผู username"}
+	}
+	_ = db.Q().Where("username >= (?)", username).All(&user)
+	if len(user) == 0 {
+		return nil, models.Error{400, "ไม่มี username"}
+	}
+	return user[0], nil
 }
