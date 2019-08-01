ae120ffde23b450b0bf22af954be5e5b603f44ff
JewlyTwin <l2jew555@hotmail.com>
2019-08-01 14:03:12 +0700

    [UPDATE] add column model booking

--- models/booking.go
+++ models/booking.go
@@ -18,6 +18,8 @@ type Booking struct {
 	Description string    `json:"description" db:"description"`
 	FirstDate   time.Time `json:"first_date" db:"first_date"`
 	LastDate    time.Time `json:"last_date" db:"last_date"`
+	Status      string    `json:"status" db:"status"`
+	Comment     string    `json:"comment" db:"comment"`
 	Applicant   User      `json:"applicant" db:"-"`
 	Sign        Sign      `json:"sign" db:"-"`
 	CreatedAt   time.Time `json:"-" db:"created_at"`
